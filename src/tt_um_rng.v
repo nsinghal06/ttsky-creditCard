f98666f77efcae4fc733acf1e6624bd6cf6c4714
nsinghal06 <nishtha.singhal07@gmail.com>
2026-02-18 13:51:31 -0500

    rename to rng

--- src/tt_um_rng.v
+++ src/tt_um_rng.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_creditCard (
+module tt_um_rng (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
